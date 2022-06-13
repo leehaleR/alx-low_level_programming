57dc97e2b72f857fa117ede5dd51c2d41809e44d
lubelihle <leehle.radebe@gmail.com>
2022-06-13 03:23:29 -0700

    print_even

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - rints every other character of a string, starting with the first character
+ * puts2 - prints every other character of a string
  * @str: char type
  *
  */
@@ -9,6 +9,7 @@
 void puts2(char *str)
 {
 	int i;
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i % 2 == 0)
