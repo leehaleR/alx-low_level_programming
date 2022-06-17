81416a2619c7fcaedfa9a1059f3043cb4af4fef5
lubelihle <leehle.radebe@gmail.com>
2022-06-17 05:00:56 -0700

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,47 +18,31 @@ char *cap_string(char *str)
 
 	char nots[] = ",;.!?(){}\n\t\" ";
 
-
-
 	for (i = 0, lee = 0; str[i] != '\0'; i++)
-
 	{
 
 		if (str[0] > 96 && str[0] < 123)
 
 			lee = 1;
-
 		for (c = 0; nots[c] != '\0'; c++)
-
 		{
-
 			if (nots[c] == str[i])
-
 				lee = 1;
-
 		}
 
-
-
 		if (lee)
-
 		{
 
 			if (str[i] > 96 && str[i] < 123)
-
 			{
-
 				str[i] -= 32;
-
 				lee = 0;
 
 			}
 
 			else if (str[i] > 64 && str[i] < 91)
-
 				lee = 0;
 			else if (str[i] > 47 && str[i] < 58)
-
 				lee = 0;
 		}
 	}
