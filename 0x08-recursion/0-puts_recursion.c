96a093db8320ca895d3556bde4aa26cffbe357d4
lubelihle <leehle.radebe@gmail.com>
2022-06-23 06:16:28 -0700

    first recursion commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,21 +1,17 @@
 #include "main.h"
 
 /**
- * puts_recursion - function that prints a string
- * @s - char type 
+ * _puts_recursion - Print a string
+ * @s: string
  */
 
 void _puts_recursion(char *s)
 {
-
 	if (*s == '\0')
-	{
 		_putchar('\n');
-	}
 	else
 	{
 		_putchar(*s);
 		_puts_recursion(++s);
 	}
-
 }
