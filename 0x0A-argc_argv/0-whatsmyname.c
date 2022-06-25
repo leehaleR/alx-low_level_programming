38e7703258905491420a9d873e964b3294cd74bb
lubelihle <leehle.radebe@gmail.com>
2022-06-25 04:31:26 -0700

    program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * main - print name of file argv c
+ * @i : integer type
+ * @argc : argument type
+ * @argv : arguments passed to main as an array of strings
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	/**If a function argument is not used
+	 */
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
